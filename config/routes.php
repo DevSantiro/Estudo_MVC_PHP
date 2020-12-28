29a8f076ba70ffca0204605521947cf77d139f24
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-12-28 14:36:17 -0300

    Primeiro Commit com algumas funções já implementadas

---
+++ config/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+switch($_SERVER['PATH_INFO']){
+    
+    case '/listar-cursos':
+        $controlador = new ListarCursos();
+        $controlador->processaRequisicao();
+        break;
+    case '/novo-curso':
+        $controlador = new FormularioInsercao();
+        $controlador->processaRequisicao();
+        break;
+    case '/salvar-curso':
+        $controlador = new Persistencia();
+        $controlador->processaRequisicao();
+        break;
+    default:
+        echo "Erro 404";
+        break;
+}
+
+*/
+
+use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\ListarCursos;
+use Alura\Cursos\Controller\Persistencia;
+
+return [
+    '/listar-cursos' => ListarCursos::class,
+    '/novo-curso' => FormularioInsercao::class,
+    '/salvar-curso' => Persistencia::class
+];
